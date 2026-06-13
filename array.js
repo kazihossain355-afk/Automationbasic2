ec22f7467761e8bcd00f3d1c30c58d58fcfb5930
kazihossain355-afk <kazihossain355@gmail.com>
2026-06-13 09:41:42 -0400

    Add array practice

---
+++ array.js
@@ -0,0 +1,10 @@
+let browser =["chrome","firefox","edge","safari"];
+
+for(const bew of browser){
+    console.log(bew);
+}
+
+for(let index =2; index < browser.length; index++){
+   
+    console.log(browser[index]);
+}
